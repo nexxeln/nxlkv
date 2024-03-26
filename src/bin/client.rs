abe725005199bb15c2b85088d66f96fa27993330
Shoubhit Dash <shoubhit2005@gmail.com>
2024-03-26 14:50:06 +0530

    sending bytes over tcp!!

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,7 +1,8 @@
-use tokio::net::TcpStream;
+use tokio::{io::AsyncWriteExt, net::TcpStream};
 
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
-    let _stream = TcpStream::connect("127.0.0.1:8081").await?;
+    let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
+    stream.write_all(b"hello world").await?;
     Ok(())
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,10 +1,14 @@
-use tokio::net::TcpListener;
+use bytes::BytesMut;
+use tokio::{io::AsyncReadExt, net::TcpListener};
 
 #[tokio::main]
 pub async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8081").await?;
+
+    let mut buf = BytesMut::with_capacity(1024);
     loop {
-        let (socket, _) = listener.accept().await?;
-        println!("connection accepted {:?}", socket);
+        let (mut socket, _) = listener.accept().await?;
+        socket.read_buf(&mut buf).await?;
+        println!("received {:?}", buf);
     }
 }
